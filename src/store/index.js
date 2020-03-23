2e12cdf5198ef9273c1e6a67a9d9c192f38e8a23
Chernyh1994 <chernuh.1994@gmail.com>
2020-03-23 18:18:58 +0200

    comment added redux-saga

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,17 @@
-import { createStore } from 'redux';
 import reducers from './reducers';
+import {createStore, applyMiddleware, compose} from 'redux'
+import createSagaMiddleware from 'redux-saga'
+import hellSagas from './sagas/sagas'
 
-export default createStore(
-  reducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(
+    reducers,
+    compose(
+        applyMiddleware(sagaMiddleware),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
 );
+
+sagaMiddleware.run(hellSagas)
+
+export default store;
---
+++ src/store/sagas/sagas.js
@@ -0,0 +1,3 @@
+export default function* hellSagas() {
+    console.log('Hello Sagas!')
+}
